ee120ec331b2c5ba00f1234f546af70137073199
Ben <ben@bencleverly.dev>
2025-09-09 22:49:49 +0100

    user form

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -22,17 +22,18 @@ class UserForm
                     ->required(),
                 DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
-                    ->password()
-                    ->required(),
+                ->password()
+                ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                ->dehydrated(fn ($state) => filled($state)),
                 TextInput::make('username'),
                 Select::make('user_type')
                     ->options(TargetUserType::class)
                     ->default(4)
                     ->required(),
-                TextInput::make('stripe_id'),
-                TextInput::make('pm_type'),
-                TextInput::make('pm_last_four'),
-                DateTimePicker::make('trial_ends_at'),
+                // TextInput::make('stripe_id'),
+                // TextInput::make('pm_type'),
+                // TextInput::make('pm_last_four'),
+                // DateTimePicker::make('trial_ends_at'),
             ]);
     }
 }
